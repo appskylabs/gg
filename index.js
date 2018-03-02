beae8f18488d857df4174cae34562195c40833da
Taylor Korensky <taylor@appskylabs.com>
2018-03-02 10:10:22 -0600

    Port
    port issue

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ router.get('/getScreenshot', (request, response) => {
 });
 
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port'));
 
